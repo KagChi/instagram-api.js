5b07c22940a0168f25f3c238e2c17a826ceac648
KagChi <59391215+KagChi@users.noreply.github.com>
2020-09-05 20:20:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const request = require("axios");
 const { author, version } = require("./package.json");
-module.exports = class InstaClient {
-  constructor() {
-    this.version = version;
-    this.author = author;
-  }
   async instagram(user) {
   if (!user) throw Error('Please enter instagram username');
     const data = await request.get("https://instagram.hanifdwyputra.xyz/?username=" + user).then(res => res.data.graphql.data);
     return data;
   };
  };
+
+module.exports = instagram;
