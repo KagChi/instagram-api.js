e4879bd07ea62025bbca2e8febd87ecd819beb05
カグチ <59391215+KagChi@users.noreply.github.com>
2021-01-30 21:38:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const request = require("axios");
-const { author, version } = require("./package.json");
   async function user(user) {
   if (!user) throw Error('Please enter instagram username');
-    const data = await request.get("https://instagram.hanifdwyputra.xyz/?username=" + user).then(res => res.data.graphql.data);
+    const data = await request.get("https://api.hansputera.me/instagram/" + user).then(res => res.data.results.graphql.user);
     return data;
   };
 
-module.exports = user;
+module.exports = { user };
