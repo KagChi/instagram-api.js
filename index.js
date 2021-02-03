dfe580d9a786e62d7e2b59db2fa1a0421bdde334
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-03 14:07:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const request = require("axios");
-  async function user(user) {
+const fetch = require("node-fetch");
+/**
+ * 
+ * @param {string} user 
+ * @param {string} sessionID 
+ */
+async function user(user, sessionID) {
   if (!user) throw Error('Please enter instagram username');
-    const data = await request.get("https://api.hansputera.me/instagram/" + user).then(res => res.data.results.graphql.user);
-    return data;
-  };
-
-module.exports = { user };
+  try {
+    var data;
+    try {
+      data = await fetch("https://api.hansputera.me/instagram/" + user).then(res => res.json());
+    } catch {
+      data = await fetch(`https://instagram.com/${user}/?__a=1`, { headers: { cookie: `sessionid=${sessionID}` } }).then(x => x.json())
+    }
+  } catch {
+    throw Error('Sorry the server is unavailable or cannot get profile match that username')
+  }
+  return data.graphql.user
+};
+module.exports = { user }
