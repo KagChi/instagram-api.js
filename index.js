229d0d7c1f7f289a1b1f33cce9db7664e857b265
KChi <59391215+KagChi@users.noreply.github.com>
2021-07-18 19:57:26 +0700

    fix: update instagram link

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const fetch = require("node-fetch");
  */
 async function user(user, sessionID) {
   if (!user) throw Error('Please enter instagram username');
+  let data;
   try {
-    var data;
-    try {
-      data = await fetch("https://api.hansputera.me/instagram/" + user).then(res => res.json());
-  } catch {
-    data = await fetch(`https://instagram.com/${user}/?__a=1`, { headers: { cookie: `sessionid=${sessionID}` } }).then(x => x.json())
-  } 
+    if(sessionID) {
+     data = await fetch(`https://instagram.com/${user}/?__a=1`, { headers: { cookie: `sessionid=${sessionID}` } }).then(x => x.json())
+   } else {
+     data = await fetch(`https://instagram.com/${user}/feed/?__a=1`).then(x => x.json())
+   }
 } catch {
     throw Error('Sorry the server is unavailable or cannot get profile match that username')
   }
